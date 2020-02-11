fa34872e4b64485fb2f0470a70798447fea1104c
krzysztof <krzysztof.kmiecik1975@gmail.com>
2020-02-11 01:29:27 +0100

    status in color

--- src/main/java/com/java26/eolt/EoltApplication.java
+++ src/main/java/com/java26/eolt/EoltApplication.java
@@ -2,12 +2,20 @@ package com.java26.eolt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EoltApplication {
+public class EoltApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EoltApplication.class, args);
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EoltApplication.class);
     }
 
+    public static void main(String[] args) {
+
+        SpringApplication.run(EoltApplication.class, args);
+    }
 }
---
+++ src/main/java/com/java26/eolt/dto/VariantSearchDto.java
@@ -0,0 +1,12 @@
+package com.java26.eolt.dto;
+
+
+import lombok.Data;
+
+@Data
+public class VariantSearchDto {
+    String eoltName;
+    String location;
+    String variantStatus;
+
+}
